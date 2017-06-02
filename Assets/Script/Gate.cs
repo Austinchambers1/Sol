a99e9b7c71f99aeacb1d247729f5d0c58ed28d1a
softkaiser <austinchambers1@gmail.com>
2017-06-01 19:56:29 -0500

    added base gate functionality

---
+++ Assets/Script/Gate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int enemyCount = 0;
+		Object[] unitList;
+		unitList = FindObjectsOfType(typeof (Attackable)) as Attackable[];
+		foreach (Attackable enemy in unitList){
+			if (enemy.faction == "enemy") {
+				enemyCount++;
+			}
+		}
+
+		if (enemyCount == 0) {
+			Debug.Log ("Open Gate");
+		}
+	}
+}
